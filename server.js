d7dfc5ecbc7f230006da3b8f75bd8f56b8644790
Sweta-364 <sweta.manjaripatel@gmail.com>
2026-01-31 11:05:05 +0530

    pass change

--- server.js
+++ server.js
@@ -8,26 +8,18 @@ app.use(express.static("public"));
 
 const PORT = 3000;
 
-/*
-  intentionally weak demo credentials
-*/
-const ADMIN_USER = "admin";
-const ADMIN_PASSWORD = "baseball";
 
-/*
-  HYDRA FRIENDLY LOGIN
-  IMPORTANT:
-  - always 200 status
-  - failure text only
-*/
+const ADMIN_USER = "admin";
+const ADMIN_PASSWORD = "batman";
+
+
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   if (username === ADMIN_USER && password === ADMIN_PASSWORD) {
     return res.redirect("/hacked.html"); // success
   }
-
-  // DO NOT send 401
+ 
   return res.send("Invalid credentials"); // 200 OK
 });
 
